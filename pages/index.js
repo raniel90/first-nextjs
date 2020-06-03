734f04455acbc97db0861f511ce21920e9120a9a
Raniel <raniel@Raniels-MacBook-Pro.local>
2020-06-02 23:49:28 -0300

    fix load data from backend

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-
-export async function getServerSideProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
+import Link from 'next/link'
+import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
-
   return (
     <Layout home>
       <Head>
@@ -23,7 +14,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -45,4 +36,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
